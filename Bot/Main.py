0f424a76ae7a0a057cdb4e70b76526164e268813
MangoKo4ek <habarovila76@gmail.com>
2025-03-07 11:39:25 +0300

    BotBuild Commit

--- Bot/Main.py
+++ Bot/Main.py
@@ -1,8 +1,7 @@
 import logging
 import asyncio
-from config import TOKEN
 from aiogram import Bot, Dispatcher
-
+from Config import TOKEN
 
 logging.basicConfig(
     format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s] %(message)s',
@@ -12,8 +11,10 @@ logging.basicConfig(
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
